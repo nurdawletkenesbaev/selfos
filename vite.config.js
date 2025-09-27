1a2154f1f4b8f4703f4f16cb958a1f48aabdfad1
nurdawletkenesbaev <nurdawletkenesbaev@gmail.com>
2025-09-28 00:06:42 +0500

    zzxc

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/', // Bu to'g'ri bo'lishi kerak
+  build: {
+    outDir: 'dist',
+  },
   server: {
     historyApiFallback: true, // faqat developmentda kerak
   },
